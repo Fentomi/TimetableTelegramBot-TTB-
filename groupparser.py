77b899754d8cd26898004311bd1f91b306041946
Fentomi <105577819+Fentomi@users.noreply.github.com>
2022-09-05 00:06:02 +0300

    Add files via upload

--- groupparser.py
+++ groupparser.py
@@ -1,31 +1,40 @@
-from multiprocessing.resource_sharer import stop
+from cgitb import html
 from bs4 import BeautifulSoup
+import time
+import os
 from datetime import date
 import requests
 
-def get_html():
-    try:
-        res = requests.get('https://ruz.narfu.ru/?timetable&group=16555')
-        if res.status_code == 200:
-            html = res.text
-            return html
-        else:
-            return ('[ERROR] Ошибка получения html страницы.')
-    except:
-        return ('[ERROR] Ошибка получения html страницы.')
+#функция обновляет каждый час на случай краха сайта index.html 
+def check_index(html): 
+    work_dir = os.getcwd() 
+    html_path = work_dir + '\site\index.html'
+    html_info =  os.lstat(html_path, dir_fd=None)
+    time_modification = html_info[8] 
+    time_current = time.time() 
+    time_difference = time_current - time_modification
+    if time_difference > 3600:
+        with open('site/index.html', 'w', encoding='utf-8') as OpenFile:
+            OpenFile.write(html)
 
-def get_soup(html):
-    soup = BeautifulSoup(html, 'lxml')
-    return soup
 
-def mainparser():
-    html = get_html()
-    if type(html) == str:
+def get_html(link): #
+    res = requests.get(link)
+    if res.status_code == 200:
+        html = res.text
+        check_index(html)
         return html
     else:
-        soup = get_soup(html)
+        with open('site/index.html', encoding='utf-8') as OpenFile:
+            html = OpenFile.read()
+            return html.text
+
+def parser(): #основное тело парсера, из которого вызываются другие функции.
+    while True:
+        html = get_html('https://ruz.narfu.ru/?timetable&group=16555') #здесь можно менять ссылку для другой группы этого же самого сайта.
+        soup = BeautifulSoup(html, 'lxml')
         day_today = date.today()
         return day_today
 
 if __name__ == '__main__':
-    mainparser()
+    parser()
--- telegram-bot.py
+++ telegram-bot.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import date
 import requests
 import groupparser
-from groupparser import mainparser
+from groupparser import parser
 from auth_data import token
 
 def telegram_bot(token):
@@ -11,7 +11,7 @@ def telegram_bot(token):
     @bot.message_handler(commands=['start']) 
     def start(message):
         bot.send_message(message.chat.id, '[INFO] Пытаюсь зайти на сайт...')
-        day_today = mainparser()
+        day_today = parser()
         print(day_today)
         bot.send_message(message.chat.id, day_today)
     bot.polling()
